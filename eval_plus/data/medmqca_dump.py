b621081fda8d3f4a980fa75f85a7203566398ca7
mina-tang <mina.tang@epfl.ch>
2025-08-07 14:29:36 +0000

    system prompt

--- eval_plus/data/medmqca_dump.py
+++ eval_plus/data/medmqca_dump.py
@@ -1,5 +1,13 @@
 from datasets import load_dataset
 import json
+# For MCQ
+SYSTEM_PROMPT = """You can add explanations or comments before or after, \
+but the text inside the <answer> tags must contain only the final MCQ answer exactly \
+as one of the provided options (e.g., "d) Roux en Y Duodenal By pass"). \
+For example:  \
+Here is my reasoning... <answer> d) Roux en Y Duodenal By pass <answer> \
+Do not include anything else inside the <answer> tags. \
+"""
 
 dataset = load_dataset("openlifescienceai/medmcqa", split="train")
 
@@ -12,5 +20,5 @@ with open("medmcqa_formatted.jsonl", "w") as f:
             f"a) {example['opa']} b) {example['opb']} c) {example['opc']} d) {example['opd']}"
         )
         answer_letter = letter_map[example["cop"]]
-        json.dump({"prompt": question_text, "answer": answer_letter}, f)
+        json.dump({"prompt": question_text + "\n\n" + SYSTEM_PROMPT, "answer": answer_letter}, f)
         f.write("\n")
